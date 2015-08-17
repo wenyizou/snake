e158bb57be7d57e2398540a463d29f535ca7b8b0
wenyizou <wenyizou@gmail.com>
2015-08-16 21:48:09 -0500

    Time and snake move function finished. Stuck in right panel position(css),can't add <div>, cause position strange problem

--- js/snake.js
+++ js/snake.js
@@ -1,12 +1,14 @@
-
-$(document).ready(function(){
-
-	//global variables for playing
-	var game_block_in_height=16;
+    var game_block_in_height=16;
 	var game_block_in_width = 20;
 	var snake_body_array = [4,3,2,1,0];
 	var bean_pos = Math.floor(Math.random()*game_block_in_height*game_block_in_width);
 	var direction = "right";
+	var difficulty = "easy";
+
+$(document).ready(function(){
+
+	//global variables for playing
+	
 	//build the grid of the game
 	for(var i = 0; i<game_block_in_height; i++){
 		for(var j = 0; j<game_block_in_width; j++){
@@ -15,20 +17,13 @@ $(document).ready(function(){
 	}
 	
 	//init the snake body and head
-	$('#b_'+ snake_head_pos).addClass('snake_head');
-	for(var i=1;i<5;i++){
-		$('#b_'+ snake_body_array[i]).addClass('snake_body');
-	}
-
 	// init the random bean position
 	// make sure bean position won't stack with snake itself
-	while(snake_body_array.indexOf(bean_pos)!==-1){
-		 bean_pos = Math.floor(Math.random()*game_block_in_height*game_block_in_width);   
-	}
-	$('#b_'+ bean_pos).addClass('bean');
+	resetBoard();
 
 	// test with user control
 	$(document).keydown(function(key){
+		// detect key press for direction change
 		switch(key.keyCode){
 			case 38 :    
 			case 87 :// up
@@ -54,11 +49,25 @@ $(document).ready(function(){
 					direction = "left";
 				}
 				break;
-			default break;   // do nothing when other key pressed
+			default :
+				break;   // do nothing when other key pressed
 		}
-		console.log("test");
-		var next_pos = snakeNextPos(snake_body_array[0],direction);
-		snakeBeanCheck(snake_body_array, next_pos, bean_pos);
+		//var next_pos = snakeNextPos(snake_body_array[0],direction);
+		//snakeBeanCheck(snake_body_array, next_pos, bean_pos);
+	});
+
+	// start game by pressing enter
+	$(document).keypress(function(key){
+		if(key.keyCode == 13){
+			if(typeof(snake_loop) == "undefined"){
+				snakeStart();
+			}else{
+				snakeStop();
+				resetBoard();
+				snakeStart();
+			}
+			
+		}
 	});
 });
 
@@ -84,7 +93,7 @@ function snakeNextPos(snake_head_pos,direction){
 			}
 			break;
 		case 'down':
-			if(snake_head_pos<game_block_in_height*game_block_in_width && snake_head_pos>=game_block_in_width*game_block_in_height-1){
+			if(snake_head_pos<game_block_in_height*game_block_in_width && snake_head_pos>=game_block_in_width*(game_block_in_height-1)){
 				return -1; // reach bottom edge, stop game
 			}else{
 				next_pos = snake_head_pos + game_block_in_width;
@@ -103,21 +112,67 @@ function snakeNextPos(snake_head_pos,direction){
 }
 
 // function for check eat bean or not
-function snakeBeanCheck(snake_body_array,next_pos,bean_pos){
+function snakeBeanCheck(snake_body_array,next_pos){
 	if(next_pos!==bean_pos){
 		// change the color of snake
 		// if not eat bean
-		$('b_'+ next_pos).addClass('snake_head');
-		$('b_'+ snake_body_array[0]).removeClass('snake_head');
-		$('b_'+ snake_body_array[0]).addClass('snake_body');
-		$('b_'+ snake_body_array[snake_body_array.length-1]).removeClass('snake_body');
+		$('#b_'+ next_pos).addClass('snake_head');
+		$('#b_'+ snake_body_array[0]).removeClass('snake_head');
+		$('#b_'+ snake_body_array[0]).addClass('snake_body');
+		$('#b_'+ snake_body_array[snake_body_array.length-1]).removeClass('snake_body');
 		snake_body_array.pop();
 		snake_body_array.unshift(next_pos);
 	}else{ // if eat the bean
-		$('b_'+ bean_pos).removeClass('bean');
-		$('b_'+ next_pos).addClass('snake_head');
-		$('b_'+ snake_body_array[0]).removeClass('snake_head');
-		$('b_'+ snake_body_array[0]).addClass('snake_body');
+		$('#b_'+ bean_pos).removeClass('bean');
+		$('#b_'+ next_pos).addClass('snake_head');
+		$('#b_'+ snake_body_array[0]).removeClass('snake_head');
+		$('#b_'+ snake_body_array[0]).addClass('snake_body');
 		snake_body_array.unshift(next_pos);
+		bean_pos = newBean(snake_body_array);
+
 	}
+}
+
+// functoin for generate new bean position
+function newBean(snake_body_array){
+	bean_pos = Math.floor(Math.random()*game_block_in_height*game_block_in_width);  
+	while(snake_body_array.indexOf(bean_pos)!==-1){
+		 bean_pos = Math.floor(Math.random()*game_block_in_height*game_block_in_width);   
+	}
+	$('#b_'+ bean_pos).addClass('bean');
+	return bean_pos;
+}
+
+// function for start the game
+function snakeStart(){	
+	snake_loop = setInterval(function(){
+		var next_pos = snakeNextPos(snake_body_array[0],direction);
+		if(next_pos == -1) {
+			snakeStop();
+			return;
+		}
+		snakeBeanCheck(snake_body_array, next_pos, bean_pos);
+	}, 200);
+}
+
+// function for stop the game
+function snakeStop(){
+	window.clearInterval(snake_loop);
+}
+
+function resetBoard(){
+	// remove all the snake and bean
+	$('#b_'+ bean_pos).removeClass('bean');
+	$('#b_'+ snake_body_array[0]).removeClass('snake_head');
+	for(var i=1; i< snake_body_array.length ;i++){
+		$('#b_'+ snake_body_array[i]).removeClass('snake_body');
+	}
+	// reset snake , bean and direction
+	snake_body_array = [4,3,2,1,0];   
+	bean_pos = newBean(snake_body_array);
+	$('#b_'+ snake_body_array[0]).addClass('snake_head');
+	for(var i=1;i<5;i++){
+		$('#b_'+ snake_body_array[i]).addClass('snake_body');
+	}
+	direction = "right";
 }
